a30281728109373b6f1021a63d3c9c31c2aa97dd
Wouter Seyen <wouter.seyen@ae.be>
2023-09-19 11:27:55 +0200

    sandwiches overview

--- src/views/SandwichesOverview.vue
+++ src/views/SandwichesOverview.vue
@@ -1,11 +1,26 @@
 <script setup lang="ts">
 import { useSandwichesStore } from '@/stores/sandwiches'
+import Card from 'primevue/card'
+import { useI18n } from 'vue-i18n'
 
 const { sandwiches } = useSandwichesStore()
+const { t } = useI18n()
 </script>
 
 <template>
-  <div>Hier komt het overzicht van de broodjes</div>
+  <div class="flex flex-wrap col-offset-3 col-6">
+    <div class="col-4 p-2" :key="sandwich.id" v-for="sandwich in sandwiches">
+      <Card>
+        <template #title>{{ sandwich.name }}</template>
+        <template #content>
+          {{ t('sandwiches.price', { price: sandwich.price }) }}
+        </template>
+        <template #footer>
+          <img :alt="sandwich.name" :src="sandwich.img" class="w-full" />
+        </template>
+      </Card>
+    </div>
+  </div>
 </template>
 
 <style scoped lang="scss"></style>
