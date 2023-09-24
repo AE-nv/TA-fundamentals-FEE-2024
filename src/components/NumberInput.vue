b451106dc4a51e3b40e62df0e9b964c6c98057a7
Wouter Seyen <wouter.seyen@ae.be>
2023-09-24 16:53:35 +0200

    nb input field

---
+++ src/components/NumberInput.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import InputNumber from 'primevue/inputnumber'
+import { computed } from 'vue'
+
+const props = defineProps<{
+  label: string
+  modelValue?: number
+  min?: number
+}>()
+
+const emit = defineEmits<{
+  'update:modelValue': [value: number | undefined]
+}>()
+
+const value = computed({
+  get() {
+    return props.modelValue
+  },
+  set(value) {
+    emit('update:modelValue', value)
+  },
+})
+</script>
+
+<template>
+  <div class="flex flex-column mt-3">
+    <label for="amount-input">{{ props.label }}</label>
+    <InputNumber
+      id="amount-input"
+      class="mt-2"
+      :model-value="value"
+      :min="props.min"
+      @input="event => value = event.value as unknown as number"
+    />
+  </div>
+</template>
+
+<style scoped lang="scss"></style>
--- src/views/SandwichDetail.vue
+++ src/views/SandwichDetail.vue
@@ -1,13 +1,15 @@
 <script setup lang="ts">
 import { useRoute } from 'vue-router'
 import { RouteParameter } from '@/router/routes'
-import { computed } from 'vue'
+import { computed, ref } from 'vue'
 import { useSandwichesStore } from '@/stores/sandwiches'
 import { useI18n } from 'vue-i18n'
+import NumberInput from '@/components/NumberInput.vue'
 
 const route = useRoute()
 const { sandwiches } = useSandwichesStore()
 const { t } = useI18n()
+const nbOfSandwiches = ref<number>()
 
 const sandwichId = route.params[RouteParameter.SandwichId] as string
 
@@ -23,7 +25,15 @@ const sandwich = computed(() => sandwiches.find((s) => `${s.id}` === sandwichId)
           <img :alt="sandwich?.name" :src="sandwich?.img" />
           <div class="mt-3">{{ sandwich?.description }}</div>
         </div>
-        <div>{{ t('sandwiches.price', { price: sandwich?.price }) }}</div>
+        <div class="flex flex-column">
+          <div>
+            {{ t('sandwiches.price', { price: sandwich?.price }) }}
+          </div>
+          <NumberInput :label="t('sandwiches.amount')" :min="1" v-model.number="nbOfSandwiches" />
+          <div v-if="nbOfSandwiches" class="mt-2">
+            {{ t('sandwiches.selected', { number: nbOfSandwiches }, { plural: nbOfSandwiches }) }}
+          </div>
+        </div>
       </div>
     </div>
   </div>
